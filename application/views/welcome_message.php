2267a637cd5bf3e9c0b0558c0b22209c55f26d41
mexzziv <ivan_me_2@hotmail.com>
2016-08-19 19:16:26 -0500

    Prier commit e inicio del proyecto

---
+++ application/views/welcome_message.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Curso de Codigo Facilito</title>
+	</head>
+	<body>
+		<div id="container">
+			<h1>hola mundo</h1>
+		</div>
+	</body>
+</html>
